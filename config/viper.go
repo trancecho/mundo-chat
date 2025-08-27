e77441c40d54e618caf96ff7edc69de1859a31a6
necolo007 <3202272848@qq.com>
2025-08-27 19:50:17 +0800

    chore:update

--- config/viper.go
+++ config/viper.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func ConfigInit() {
@@ -11,6 +12,10 @@ func ConfigInit() {
 	viper.AddConfigPath("./config")
 	err := viper.ReadInConfig()
 	if err != nil {
-		log.Println("Fatal error config file: ./config/app.yaml")
+		log.Println("Error reading config file:", err)
+		//检查配置文件是否存在
+		if _, statErr := os.Stat("./config/config.yaml"); os.IsNotExist(statErr) {
+			log.Println("./config/config.yaml not found")
+		}
 	}
 }
